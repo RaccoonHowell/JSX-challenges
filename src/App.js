34306316c106b62ef3bee13f33c8476cdc88b0fb
Rachel <rac777blue@hotmail.com>
2020-03-02 16:51:34 +0000

    updated the Stuff component finishing the Props challenges

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Stuff from './Stuff';
 
 const App = () => (
-    <Stuff />
+    <Stuff square={true}/>
   );
 
 export default App;
--- src/Stuff.js
+++ src/Stuff.js
@@ -4,11 +4,11 @@ import Paragraph from './Paragraph';
 import Square from './Square';
 import People from './People';
 
-const Stuff = () => (
+const Stuff = ({square}) => (
     <React.Fragment>
         <Header text='Hello, world!'/>
         <Paragraph />
-        <Square />
+        {square ? <Square /> : null}
         <People names={["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"]}/>
     </React.Fragment>
 );
@@ -16,3 +16,5 @@ const Stuff = () => (
 export default Stuff;
 
 // Create a component <Stuff> that includes all of the components so far and then use it in your <App> component:
+
+// Update your <Stuff> component so that it can accept a square prop, which will determine whether the <Square> component is displayed or not. This prop should default to true.
